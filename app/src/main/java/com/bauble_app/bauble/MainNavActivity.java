8c7e3c6e9323e5ac576b47343d1fe8b8aeb5a03d
chrisli8 <8chrisgli@gmail.com>
2017-04-02 16:48:55 -0700

    Front page layout completed with place holder images

--- app/src/main/java/com/bauble_app/bauble/MainNavActivity.java
+++ app/src/main/java/com/bauble_app/bauble/MainNavActivity.java
@@ -3,6 +3,7 @@ package com.bauble_app.bauble;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
@@ -10,6 +11,7 @@ import android.widget.TextView;
 public class MainNavActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
+    private FragmentManager fragManager;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -19,12 +21,18 @@ public class MainNavActivity extends AppCompatActivity {
             switch (item.getItemId()) {
                 case R.id.navigation_front:
                     mTextMessage.setText(R.string.title_front);
+                    fragManager.beginTransaction()
+                            .replace(R.id.content, new FrontFragment())
+                            .commit();
                     return true;
                 case R.id.navigation_explore:
                     mTextMessage.setText(R.string.title_explore);
                     return true;
                 case R.id.navigation_create:
                     mTextMessage.setText(R.string.title_create);
+                    fragManager.beginTransaction()
+                            .replace(R.id.content, new CreateFragment())
+                            .commit();
                     return true;
                 case R.id.navigation_me:
                     mTextMessage.setText(R.string.title_me);
@@ -42,6 +50,11 @@ public class MainNavActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_nav);
+
+        fragManager = getSupportFragmentManager();
+        fragManager.beginTransaction()
+                .replace(R.id.content, new FrontFragment())
+                .commit();
 
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
