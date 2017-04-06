4682679147b0350c3611bb575e0092c4d90d3453
chrisli8 <8chrisgli@gmail.com>
2017-04-06 15:05:41 -0700

    pure ui for front and create

--- app/src/main/java/com/bauble_app/bauble/MainNavActivity.java
+++ app/src/main/java/com/bauble_app/bauble/MainNavActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
