4602240bced9ebf83fd47ce6e35e6ee38143064f
chrisli8 <8chrisgli@gmail.com>
2017-05-29 00:45:04 -0700

    adding emojis to stories now work

--- app/src/main/java/com/bauble_app/bauble/ViewFragment.java
+++ app/src/main/java/com/bauble_app/bauble/ViewFragment.java
@@ -1,8 +1,11 @@
 package com.bauble_app.bauble;
 
 
+import android.app.Dialog;
+import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.media.MediaPlayer;
 import android.os.Bundle;
@@ -13,10 +16,15 @@ import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.HorizontalScrollView;
 import android.widget.ImageButton;
 import android.widget.ImageView;
@@ -70,6 +78,7 @@ public class ViewFragment extends Fragment {
     private ProgressBar loading;
 
     private ImageButton mReplyButton;
+    private ImageButton mReactButton;
     private FragmentManager mFragManager;
     private MediaPlayer mPlayer;
     private StorySingleton mStorySingleton;
@@ -83,6 +92,7 @@ public class ViewFragment extends Fragment {
 
     private OnSuccessListener audioLoading;
     private StorageReference audioPathReference;
+    private LinearLayout emojiChain;
 
     public ViewFragment() {
         // Required empty public constructor
@@ -105,14 +115,14 @@ public class ViewFragment extends Fragment {
         for (int i = 600; i <= 637; i++) {
             emojiSource.add("emoji_1f" + i);
         }
-        LinearLayout emojiChain = (LinearLayout) v.findViewById(R.id.view_emoji_chain);
-        for (String code : emojiSource) {
-            ImageView emoji = new ImageView(getContext());
-            emoji.setLayoutParams(new LinearLayout.LayoutParams(100, 100));
-            emoji.setPadding(4, 4, 4, 4);
-            emoji.setImageResource(getResources().getIdentifier(code, "drawable", "com.bauble_app.bauble"));
-            emojiChain.addView(emoji);
-        }
+        emojiChain = (LinearLayout) v.findViewById(R.id.view_emoji_chain);
+//        for (String code : emojiSource) {
+//            ImageView emoji = new ImageView(getContext());
+//            emoji.setLayoutParams(new LinearLayout.LayoutParams(60, 60));
+//            emoji.setPadding(4, 4, 4, 4);
+//            emoji.setImageResource(getResources().getIdentifier(code, "drawable", "com.bauble_app.bauble"));
+//            emojiChain.addView(emoji);
+//        }
 
         // Hide waveforms initially until loading complete
         waveforms = (ImageView) v.findViewById(R.id.view_waveforms);
@@ -140,6 +150,15 @@ public class ViewFragment extends Fragment {
                         createFrag, "REPLY_FRAG").commit();
 
                 System.out.println("replybtn2");
+            }
+        });
+
+        // set up reach button
+        mReactButton = (ImageButton) v.findViewById(R.id.view_btn_react);
+        mReactButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showMyDialog(getContext(), v);
             }
         });
 
@@ -350,7 +369,7 @@ public class ViewFragment extends Fragment {
 
                 de.hdodenhof.circleimageview.CircleImageView child = new de.hdodenhof.circleimageview.CircleImageView(getContext());
                 LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(200, 200);
-                lp.setMargins(0, 0, 100, 0);
+                lp.setMargins(0, 0, 60, 0);
                 child.setLayoutParams(lp);
                 child.setOnClickListener(new View.OnClickListener() {
                     @Override
@@ -639,6 +658,76 @@ public class ViewFragment extends Fragment {
             mPlayer.release();
             mPlayer = null;
         }
+    }
+
+    // Call to show custom dialog
+    private void showMyDialog(Context context, View v) {
+        final Dialog dialog = new Dialog(context);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.emoji_dialog);
+        dialog.setCanceledOnTouchOutside(true);
+        dialog.setCancelable(true);
+
+
+        List<ImageView> emojis = new ArrayList<ImageView>();
+        emojis.add((ImageView) dialog.findViewById(R.id.emoji_btn1));
+        emojis.add((ImageView) dialog.findViewById(R.id.emoji_btn2));
+        emojis.add((ImageView) dialog.findViewById(R.id.emoji_btn3));
+        emojis.add((ImageView) dialog.findViewById(R.id.emoji_btn4));
+        emojis.add((ImageView) dialog.findViewById(R.id.emoji_btn5));
+        emojis.add((ImageView) dialog.findViewById(R.id.emoji_btn6));
+
+        for (ImageView imageView : emojis) {
+            imageView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    ImageView img = (ImageView) v;
+                    Drawable emojiDrawable = img.getDrawable();
+                    ImageView emoji = new ImageView(getContext());
+                    emoji.setLayoutParams(new LinearLayout.LayoutParams(60, 60));
+                    emoji.setPadding(4, 4, 4, 4);
+                    emoji.setImageDrawable(emojiDrawable);
+                    emojiChain.addView(emoji);
+                    dialog.dismiss();
+                }
+            });
+        }
+
+//        Button btnBtmLeft = (Button) dialog.findViewById(R.id.btnBtmLeft);
+//        Button btnBtmRight = (Button) dialog.findViewById(R.id.btnBtmRight);
+//
+//        btnBtmLeft.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                dialog.dismiss();
+//            }
+//        });
+//
+//        btnBtmRight.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                // do whatever you want here
+//            }
+//        });
+
+        /**
+         * if you want the dialog to be specific size, do the following
+         * this will cover 85% of the screen (95% width and 33% height)
+         */
+        DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
+        int dialogWidth = (int)(displayMetrics.widthPixels * 0.95);
+        int dialogHeight = (int)(displayMetrics.heightPixels * 0.10);
+        dialog.getWindow().setLayout(dialogWidth, dialogHeight);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+
+        Window window = dialog.getWindow();
+        WindowManager.LayoutParams wlp = window.getAttributes();
+
+        wlp.y = 400;
+        // wlp.flags &= ~WindowManager.LayoutParams.FLAG_DIM_BEHIND;
+        window.setAttributes(wlp);
+
+        dialog.show();
     }
 
 }
