afffc753e801a900ce28ec04f8d2b64cb1a67917
chrisli8 <8chrisgli@gmail.com>
2017-05-31 13:00:14 -0700

    dialog for donating added, progress bar updated

--- app/src/main/java/com/bauble_app/bauble/FrontFragment.java
+++ app/src/main/java/com/bauble_app/bauble/FrontFragment.java
@@ -1,13 +1,23 @@
 package com.bauble_app.bauble;
 
 
+import android.app.Dialog;
+import android.content.Context;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
@@ -22,6 +32,8 @@ import com.google.firebase.storage.StorageReference;
 
 import org.w3c.dom.Text;
 
+import java.io.File;
+
 
 /**
  * A simple {@link Fragment} subclass.
@@ -35,6 +47,10 @@ public class FrontFragment extends Fragment {
     public TextView expireView;
     public TextView playView;
 
+    public com.bauble_app.bauble.CustomButton saveCollection;
+    public ProgressBar donateBar;
+    public TextView donateAmount;
+
     public FrontFragment() {
         // Required empty public constructor
     }
@@ -46,6 +62,17 @@ public class FrontFragment extends Fragment {
 
         View v = inflater.inflate(R.layout.fragment_front, container,
                 false);
+
+        saveCollection = (com.bauble_app.bauble.CustomButton) v.findViewById(R.id.community_collect);
+        saveCollection.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showSaveDialog(getContext(), v);
+            }
+        });
+
+        donateBar = (ProgressBar) v.findViewById(R.id.community_donate_progress);
+        donateAmount = (TextView) v.findViewById(R.id.community_amount);
 
 //        FirebaseStorage storage = FirebaseStorage.getInstance();
 //        // Reference to an image file in Firebase Storage
@@ -101,6 +128,57 @@ public class FrontFragment extends Fragment {
 
         // Inflate the layout for this fragment
         return v;
+    }
+
+    // Call to show custom dialog
+    private void showSaveDialog(Context context, View v) {
+        final Dialog dialog = new Dialog(context, R.style.PauseDialog);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.capstone_save_dialog);
+        dialog.setCanceledOnTouchOutside(true);
+        dialog.setCancelable(true);
+
+        Button btnBtmLeft = (Button) dialog.findViewById(R.id.btnBtmLeft);
+        Button btnBtmRight = (Button) dialog.findViewById(R.id.btnBtmRight);
+
+        // Donate
+        btnBtmLeft.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int progress = donateBar.getProgress();
+                donateBar.setProgress(progress + 1);
+                int amount = Integer.parseInt(donateAmount.getText().toString());
+                donateAmount.setText("" + (amount + 10));
+                dialog.dismiss();
+            }
+        });
+
+        btnBtmRight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+                // do whatever you want here
+            }
+        });
+
+        /**
+         * if you want the dialog to be specific size, do the following
+         * this will cover 85% of the screen (95% width and 33% height)
+         */
+        DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
+        int dialogWidth = (int)(displayMetrics.widthPixels * 0.95);
+        int dialogHeight = (int)(displayMetrics.heightPixels * 0.45);
+        dialog.getWindow().setLayout(dialogWidth, dialogHeight);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+
+        Window window = dialog.getWindow();
+        WindowManager.LayoutParams wlp = window.getAttributes();
+
+        wlp.y = 0;
+        // wlp.flags &= ~WindowManager.LayoutParams.FLAG_DIM_BEHIND;
+        window.setAttributes(wlp);
+
+        dialog.show();
     }
 
 }
